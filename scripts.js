56b39085e1f9239cc783750f06b9ea92ab992eca
CaioGabriel007 <caiogabrielbatera7500@gmail.com>
2026-09-14 21:54:36 -0300

    Commit Projeto Mário

---
+++ scripts.js
@@ -0,0 +1,23 @@
+
+const formulario = document.querySelector(".formulario")
+const mascara = document.querySelector(".mascara-formulario")
+
+
+function mostrarForm() {
+    formulario.style.left = "50%";
+    formulario.style.transform = "translateX(-50%)";
+    mascara.style.visibility = "visible";
+}
+
+function esconderForm() {
+    formulario.style.left = "-340px";
+    formulario.style.transform = "translateX(-50%)";
+    mascara.style.visibility = "hidden";
+}
+
+
+function mostrarFormAlt() {
+    formulario.style.left = "50%";
+    formulario.style.transform = "translateX(-50%)";
+    mascara.style.visibility = "visible";
+}
